ad0ba4176266975d5b99ef8b1b070ce230e30b97
Jarne Kerkaert <jarnekerkaert@gmail.com>
2020-07-02 21:49:02 +0200

    React setup  * Create footer and header components  * react eslint setup

---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Header from "../common/components/Header/Header";
+import Footer from "../common/components/Footer/Footer";
+
+const App: React.FC = () => {
+    return (<>
+        <Header/>
+        <Footer/>
+    </>);
+};
+
+export default App;
