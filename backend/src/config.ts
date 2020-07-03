4c62262f4007fabfc738183af1275b877eb93bc0
angelocarly <angelo.carly@protonmail.com>
2020-07-03 22:43:38 +0200

    Migrate database on start

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -2,6 +2,7 @@ import { ConnectionOptions } from "typeorm";
 
 import { Category } from "./entities/category.entity";
 import { Consumption } from "./entities/consumption.entity";
+import { join } from "path";
 
 const typeOrmConfig: ConnectionOptions = {
 	type: "sqlite",
@@ -11,7 +12,11 @@ const typeOrmConfig: ConnectionOptions = {
 	entities: [
 		Category,
 		Consumption
-	]
+	],
+	migrations: [
+		join(__dirname, "./migrations/{.ts,*.js}")
+	],
+	migrationsRun: true
 };
 
 export { typeOrmConfig };
