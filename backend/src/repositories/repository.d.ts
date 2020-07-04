ca8804d1dfd886caa1e04eb63abc201697b75895
Jarne Kerkaert <jarnekerkaert@gmail.com>
2020-07-04 18:18:46 +0200

    Linting

--- backend/src/repositories/repository.d.ts
+++ backend/src/repositories/repository.d.ts
@@ -3,7 +3,7 @@ import { DeleteResult, InsertResult, UpdateResult } from "typeorm";
 export interface GenericRepository<T> {
 	findAll(): Promise<T[]>;
 	insert(t: T): Promise<InsertResult>;
-	find(id: number): Promise<T >;
+	find(id: number): Promise<T>;
 	update(id: number, t: T): Promise<UpdateResult>;
 	delete(id: number): Promise<DeleteResult>;
 }
