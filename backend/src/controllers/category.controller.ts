f649dba84e214448b4b889bb6402fb1100968d3a
Jarne Kerkaert <jarnekerkaert@gmail.com>
2020-07-09 00:34:14 +0200

    Merge branch 'development' of github.com:angelocarly/Excuze into feature/orders
    # Conflicts:
    #	backend/src/config.ts
    #	backend/src/controllers/category.controller.ts
    #	backend/src/controllers/menu.controller.ts
    #	backend/src/controllers/user.controller.ts
    #	backend/src/models/dto/user.dto.ts
    #	backend/src/models/entities/user.entity.ts
    #	backend/src/repositories/user.repository.ts
    #	backend/src/server.ts
    #	backend/src/services/consumption.service.ts
    #	backend/src/services/service.d.ts
    #	backend/src/services/user.service.ts

--- backend/src/controllers/category.controller.ts
+++ backend/src/controllers/category.controller.ts
@@ -2,6 +2,7 @@ import { Inject } from "typedi";
 import CategoryService from "../services/category.service";
 import { Category } from "../models/entities/category.entity";
 import { Authorized, Body, Delete, Get, JsonController, Param, Post, Put } from "routing-controllers";
+import { DeleteResult, InsertResult, UpdateResult } from "typeorm";
 
 @JsonController()
 export class CategoryController {
@@ -10,31 +11,31 @@ export class CategoryController {
 	private categoryService!: CategoryService;
 
 	@Get("/categories")
-	async getAll() {
+	async getAll(): Promise<Category[]> {
 		return await this.categoryService.findAll();
 	}
 
 	@Get("/categories/:id")
-	async getOne(@Param("id") id: number) {
+	async getOne(@Param("id") id: number): Promise<Category> {
 		return await this.categoryService.find(id);
 	}
 
 	@Authorized()
 	@Post("/categories")
-	async post(@Body() category: Category) {
+	async post(@Body() category: Category): Promise<InsertResult> {
 		return await this.categoryService.insert(category);
 	}
 
 	@Authorized()
 	@Put("/categories/:id")
-	async put(@Param("id") id: number, @Body() category: Category) {
+	async put(@Param("id") id: number, @Body() category: Category): Promise<UpdateResult> {
 		category.id = id;
 		return await this.categoryService.update(id, category);
 	}
 
 	@Authorized()
 	@Delete("/categories/:id")
-	async remove(@Param("id") id: number) {
+	async remove(@Param("id") id: number): Promise<DeleteResult> {
 		return await this.categoryService.delete(id);
 	}
 
