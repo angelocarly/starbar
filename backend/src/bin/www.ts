765d0ee25fc54f1899937e8c6e16a17bb4779f5d
angelocarly <angelo.carly@protonmail.com>
2020-07-21 19:17:57 +0200

    Remove old production code

--- backend/src/bin/www.ts
+++ backend/src/bin/www.ts
@@ -3,8 +3,6 @@ import { PDFTicketService } from "../services/ticket.service";
 import { createConnection } from "typeorm";
 import { typeOrmConfig } from "../config";
 import server from "../server";
-import express from "express";
-import path from "path";
 
 // Verify the print mode
 switch (process.env.PRINT_MODE) {
@@ -18,17 +16,6 @@ case "print":
 	break;
 default:
 	throw new Error("Please provide PRINT_MODE in '.env'. Possible values (pdf, print)");
-}
-
-// Provide frontend in production
-if ( process.env.ENV === "prod" ) {
-	server.use(express.static(path.join(__dirname, "../../frontend/build")));
-	server.get("/", (
-		req: express.Request,
-		res: express.Response
-	) => {
-		res.sendFile(path.join(__dirname, "../../frontend/build", "index.html"));
-	});
 }
 
 export default server;
