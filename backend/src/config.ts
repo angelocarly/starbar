d8fe3c0488e6f4a3b4908dd4ef2deaf38098e209
angelocarly <angelo.carly@protonmail.com>
2020-09-12 02:45:42 +0200

    Add database config

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -6,6 +6,7 @@ const typeOrmConfig: ConnectionOptions = {
 	port: Number(process.env.DB_PORT) || 5432,
 	username: process.env.DB_USERNAME || "root",
 	password: process.env.DB_PASSWORD || "admin",
+	database: process.env.DB_NAME || "development",
 	logging: false,
 	entities: [`${__dirname}/models/entities/index{.ts,*.js}`],
 	migrations: [`${__dirname}/db/migrations/{.ts,*.js}`],
