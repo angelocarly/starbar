9d7533568e463f4cef351fbb095cea847757ab42
angelocarly <angelo.carly@protonmail.com>
2020-09-12 02:41:02 +0200

    Fix config

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,9 +1,9 @@
 import { ConnectionOptions } from "typeorm";
 
 const typeOrmConfig: ConnectionOptions = {
-	type: process.env.DB_TYPE || "postgres",
+	type: "postgres",
 	host: process.env.DB_HOST || "localhost",
-	port: process.env.DB_PORT || 5432,
+	port: Number(process.env.DB_PORT) || 5432,
 	username: process.env.DB_USERNAME || "root",
 	password: process.env.DB_PASSWORD || "admin",
 	logging: false,
