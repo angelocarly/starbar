834bf7bd99e1869b9fc1e50b986e0b0e30991f6d
angelocarly <angelo.carly@protonmail.com>
2020-09-12 02:38:40 +0200

    Add config env

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,11 +1,11 @@
 import { ConnectionOptions } from "typeorm";
 
 const typeOrmConfig: ConnectionOptions = {
-	type: "postgres",
-	host: "localhost",
-	port: 5432,
-	username: "root",
-	password: "admin",
+	type: process.env.DB_TYPE || "postgres",
+	host: process.env.DB_HOST || "localhost",
+	port: process.env.DB_PORT || 5432,
+	username: process.env.DB_USERNAME || "root",
+	password: process.env.DB_PASSWORD || "admin",
 	logging: false,
 	entities: [`${__dirname}/models/entities/index{.ts,*.js}`],
 	migrations: [`${__dirname}/db/migrations/{.ts,*.js}`],
