89344e7a7fd1fd088a3537f0d3caa15d851d89f2
angelocarly <angelo.carly@protonmail.com>
2020-09-17 13:01:26 +0200

    Remove eslint react dependency

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
 		"eslint:recommended",
 		"plugin:react/recommended",
 		"plugin:@typescript-eslint/eslint-recommended",
-		"react-scripts",
 	],
 	globals: {
 		Atomics: "readonly",
